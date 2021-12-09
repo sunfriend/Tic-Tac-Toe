c792b8dd4d122c8867c8d0b1f19f20a15d8e13d2
Yu Nak <sunfriendo@gmail.com>
2021-12-08 20:18:32 -0600

    Fx s

--- scripts/main.js
+++ scripts/main.js
@@ -272,11 +272,11 @@ function displayRoundSummary(player, symbol) {
     summaryWindow.querySelector(".summary-content").style.visibility = "visible";
     if (player) {
         summaryWindow.querySelector("h2").innerText = `${player.getName()} won round!`;
-        summaryWindow.querySelector("img").src = symbol === "X" ? "[[Tic-Tac-Toe/img/X.png|X_Image]]" : "[[Tic-Tac-Toe/img/O.png|Y_Image]]";
+        summaryWindow.querySelector("img").src = symbol === "X" ? "[[../img/X.png|X_Image]]" : "[[../img/O.png|Y_Image]]";
     }
     else {
         summaryWindow.querySelector("h2").innerText = "Yep, it's a tie :)";
-        summaryWindow.querySelector("img").src = "../img/tie.png";
+        summaryWindow.querySelector("img").src = "[[../img/tie.png]]";
     }
     summaryWindow.querySelector("button").addEventListener("click", closeWindow);
     summaryWindow.querySelector("button:last-child").addEventListener("click", startNewGame);
